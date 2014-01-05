0525f17e759802da258c09ebb3e573449b38ba67
Christoph Burschka <christoph@burschka.de>
2014-01-05 04:39:34 +0100

    Save code by white-listing commands for online and offline usage.

--- js/chat.js
+++ js/chat.js
@@ -1,10 +1,6 @@
 var chat = {
   commands: {
     connect: function(arg) {
-      if (xmpp.status != 'offline') {
-        console.log(xmpp.status);
-        return ui.messageAddError('Error: You must disconnect first.');
-      }
       var args = /^([^\s"&'\/:<>@]*)(.*)$/.exec(arg.trim());
       var user = args[1];
       var pass = args[2].trim();
@@ -16,21 +12,21 @@ var chat = {
       xmpp.newConnection(user, pass);
     },
     quit: function(arg) {
-      if (xmpp.status != 'online') {
-        return ui.messageAddError('Error: You are not connected.');
-      }
       xmpp.disconnect();
     },
     nick: function(arg) {
-      if (xmpp.status != 'online') {
-        return ui.messageAddError('Error: You are not connected.');
-      }
       var nick = arg.trim();
       if (! /^[^\s]+$/.exec(nick)) {
         return ui.messageAddError('Error: Nicknames cannot contain spaces.');
       }
       xmpp.changeNick(nick);
     }
+  },
+
+  cmdAvailableStatus: {
+    online: ['join', 'quit', 'nick'],
+    offline: ['connect'],
+    waiting: [],
   },
 
   executeInput: function(text) {
@@ -55,6 +51,9 @@ var chat = {
 
   executeCommand: function(cmd, arg) {
     if (this.commands[cmd]) {
+      if (this.cmdAvailableStatus[xmpp.status].indexOf(cmd) < 0) {
+        return ui.messageAddError('/' + cmd + ' command not available while ' + xmpp.status);
+      }
       this.commands[cmd](arg);
     }
     else {
