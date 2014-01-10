3948074e95c6137c9ff1a8ecbe3929b3258f3d3a
Christoph Burschka <christoph@burschka.de>
2014-01-10 13:31:40 +0100

    Add some comments to chat.js

--- js/core/chat.js
+++ js/core/chat.js
@@ -1,12 +1,39 @@
+/**
+ * chat.js contains all the functions that alter the state
+ * in response to user requests.
+ *
+ * @author Christoph Burschka <christoph@burschka.de>
+ * @year 2014
+ * @license GPL3+
+ */
 var chat = {
+  /**
+   * All commands executable in chat by prefixing them with '/'.
+   */
   commands: {
+    /**
+     * away <msg>:
+     *   Send a room presence with <show/> set to "away" and
+     *   <status/> to "msg".
+     */
     away: function(arg) {
       arg = arg.trim().match(/^\(*(.*?)\)*$/);
       xmpp.sendStatus('away', arg[1].trim());
     },
+
+    /**
+     * back <msg>:
+     *   Send an empty room presence that unsets <show/> and <status/>.
+     */
     back: function() {
       xmpp.sendStatus();
     },
+
+    /**
+     * connect <user> <pass>
+     * connect {user:<user>, pass:<pass>}
+     *   Open a connection and authenticate.
+     */
     connect: function(arg) {
       if (typeof arg == 'string') {
         var m = /^([^\s"&'\/:<>@]*)(.*)$/.exec(arg.trim());
@@ -19,13 +46,29 @@ var chat = {
       }
       xmpp.newConnection(arg.user, arg.pass);
     },
+
+    /**
+     * me <msg>
+     *   Alias for /say "/me <msg>".
+     */
     me: function(arg) {
       chat.sendMessage('/me ' + arg); // XEP-0245 says to send this in plain.
     },
+
+    /**
+     * nick <nick>
+     *   Ask XMPP to change the nick in the current room.
+     */
     nick: function(arg) {
       var nick = visual.lengthLimit(visual.textPlain(arg.trim().replace(/ /g, '_')), config.ui.maxNickLength);
       xmpp.changeNick(nick);
     },
+
+    /**
+     * join <room>
+     *   Ask XMPP to join <room>. If successful, XMPP
+     *   will automatically leave the current room.
+     */
     join: function(arg) {
       var room = arg.trim();
       if (xmpp.room.current == room) {
@@ -34,27 +77,47 @@ var chat = {
       xmpp.joinRoom(room);
       chat.setSetting('xmpp.room', room);
     },
+
+    /**
+     * quit
+     *   Ask XMPP to disconnect.
+     */
+    quit: function(arg) {
+      xmpp.disconnect();
+    },
+
+    /**
+     * say <msg>
+     *   The default command that simply sends a message verbatim.
+     */
     say: function(arg) {
       arg = visual.lengthLimit(visual.textPlain(arg), config.ui.maxMessageLength);
       chat.sendMessage(arg);
     },
-    quit: function(arg) {
-      xmpp.disconnect();
-    }
   },
 
+  /**
+   * List the commands available by connection state.
+   * This saves having to check the connection state in
+   * each command handler.
+   */
   cmdAvailableStatus: {
     online: ['away', 'back', 'join', 'me', 'nick', 'quit', 'say'],
     offline: ['connect'],
     waiting: [],
   },
 
+  /**
+   * Parse input sent by the user and execute the appropriate command.
+   */
   executeInput: function(text) {
     text = text.trim();
     if (!text) return;
 
+    // Without commands, execute /say.
     var cmd = 'say';
 
+    // Execute /cmd, but turn //cmd into /say /cmd.
     if (text[0] == '/') {
       if (text[1] != '/') {
         var i = text.indexOf(' ');
@@ -76,6 +139,9 @@ var chat = {
     }
   },
 
+  /**
+   * Ask XMPP to send a message to the current room.
+   */
   sendMessage: function(text) {
     html = bbcode.render(text);
     if (config.settings.textColor) {
@@ -84,6 +150,17 @@ var chat = {
     xmpp.sendMessage(html);
   },
 
+  /**
+   * Insert a text into the input field.
+   * @param {string} text The text to insert.
+   * @param {array} text The beginning and end tags to insert.
+   *
+   * If an array is given, then it will be wrapped around the selected
+   * text. A string will replace the selected text.
+   * If an array is given and no text is selected, the cursor will
+   * be moved between the tags. Otherwise it will be moved to the end
+   * of the inserted text.
+   */
   insertText: function(text) {
     ui.dom.inputField.focus();
     var inputFieldJQ = ui.dom.inputField;
@@ -99,6 +176,10 @@ var chat = {
     inputField.selectionEnd = end;
   },
 
+  /**
+   * Take a dotted string and return the respective value
+   * in the settings dictionary.
+   */
   getSetting: function(key) {
     var path = key.split('.');
     var ref = config.settings;
@@ -108,6 +189,10 @@ var chat = {
     return ref;
   },
 
+  /**
+   * Take a dotted string and set that settings key to the
+   * given value. Immediately saves.
+   */
   setSetting: function(key, val) {
     var path = key.split('.');
     var ref = config.settings;
@@ -118,6 +203,9 @@ var chat = {
     this.saveSettings();
   },
 
+  /**
+   * Serialize the settings object and save it in the cookie.
+   */
   saveSettings: function() {
     $.cookie(config.sessionName + '_settings', config.settings);
   }
