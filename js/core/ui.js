47de71740704ec6507b19a1809fb25b56b05b0c7
Christoph Burschka <christoph@burschka.de>
2014-01-15 07:19:51 +0100

    comments added to ui.js (fixes #22)

--- js/core/ui.js
+++ js/core/ui.js
@@ -1,3 +1,10 @@
+/**
+ * ui.js contains all functions that alter the user interface.
+ *
+ * @author Christoph Burschka <christoph@burschka.de>
+ * @year 2014
+ * @license GPL3+
+ */
 var ui = {
   userLinks: {},
   dom: null,
@@ -8,6 +15,12 @@ var ui = {
   colorPicker: null,
   autoScroll: true,
 
+  /**
+   * Initialize the module:
+   * - store references to frequently accessed DOM elements.
+   * - build dynamically generated elements, and set form values.
+   * - initialize event listeners.
+   */
   init: function() {
     this.dom = {
       loginContainer: $('#loginContainer'),
@@ -32,7 +45,12 @@ var ui = {
     this.initializeEvents();
   },
 
+  /**
+   * Create dynamic page elements.
+   */
   initializePage: function() {
+
+    // Build the emoticon containers.
     for (var set in config.markup.emoticons) {
       var html = '';
       for (var code in config.markup.emoticons[set].codes) {
@@ -44,6 +62,8 @@ var ui = {
       }
       $('#emoticonsList-' + set).html(html);
     }
+
+    // Build the color palette picker.
     var html = '';
     for (var color in config.markup.colorCodes) {
       var code = config.markup.colorCodes[color]
@@ -52,6 +72,7 @@ var ui = {
     }
     $('#colorCodesContainer').html(html);
 
+    // Build the stylesheet menu and the stylesheet links.
     var options = '', links = '';
     for (var i in config.ui.css) {
       options += '<option value="' + config.ui.css[i] + '">' + config.ui.css[i] + '</option>';
@@ -60,11 +81,14 @@ var ui = {
     }
     $('#styleSelection').html(options).val(config.settings.activeStyle);
     this.dom.styleSheets = $(links).appendTo('head');
+    // Once all stylesheets have loaded, fade in the page.
     this.dom.styleSheets.last().load(function() {
       ui.setStyle(config.settings.activeStyle);
       $('#nocontent').fadeOut('slow');
       $('#content').fadeIn('slow');
     });
+
+    // Set the form values.
     $('#settingsContainer input.settings').val(function() {
       return chat.getSetting(this.id.substring('settings-'.length));
     });
@@ -72,12 +96,20 @@ var ui = {
     $('#settingsContainer input.settings[type=checkbox]').prop('checked', function() {
       return chat.getSetting(this.id.substring('settings-'.length));
     });
+
+    // Open the last active sidebar.
     this.toggleMenu(config.settings.activeMenu, true);
   },
 
+  /**
+   * Initialize the event listeners.
+   */
   initializeEvents: function() {
+
+    // The input field listens for three keystrokes.
     this.dom.inputField.on({
       keypress: this.onKeyMap({
+        // 13: <enter> (unless shift is down)
         13: function(e,x) {
           if (!e.shiftKey) {
             chat.executeInput($(x).val())
@@ -85,16 +117,22 @@ var ui = {
             return true;
           }
         },
+        // 38: <arrow-up> (if the field is empty, or ctrl is down)
         38: function(e,x) { return (e.ctrlKey || !$(x).val()) && chat.historyUp(); },
+        // 40: <arrow-down> (if ctrl is down)
         40: function(e) { return e.ctrlKey && chat.historyDown(); }
       }),
+      // after any keystroke, update the message length counter.
       keyup: function() { ui.updateMessageLengthCounter(); }
     });
+
+    // The room selection menu listens for changes.
     this.dom.roomSelection.change(function() {
       if (this.value) chat.commands.join(this.value);
       else chat.commands.part();
     });
 
+    // Log in with the button or pressing enter.
     var loginCallback = function() {
       chat.commands.connect({user: $('#loginUser').val(), pass: $('#loginPass').val()});
     };
@@ -107,12 +145,12 @@ var ui = {
       ui.toggleMenu(this.id.substring(0, this.id.length - 'Button'.length));
     });
 
+    // Inserting BBCode tags.
     var insertBBCode = function(tag, arg) {
       arg = arg ? '=' + arg : '';
       var v = ['[' + tag + arg + ']', '[/' + tag + ']'];
       chat.insertText(v);
     };
-
     $('.insert-text').click(function() { chat.insertText(this.title); });
     $('.insert-bbcode').click(function() {
       if ($(this).hasClass('insert-bbcode-arg'))
@@ -120,6 +158,7 @@ var ui = {
       insertBBCode(this.value.toLowerCase(), arg || '');
     });
 
+    // Open the color tray.
     $('#colorBBCode').click(function() {
       ui.colorPicker = ui.colorPicker != 'bbcode' ? 'bbcode' : null;
       ui.dom.colorCodesContainer[ui.colorPicker == 'bbcode' ? 'fadeIn' : 'fadeOut'](500);
@@ -128,6 +167,8 @@ var ui = {
       ui.colorPicker = ui.colorPicker != 'setting' ? 'setting' : null;
       ui.dom.colorCodesContainer[ui.colorPicker == 'setting' ? 'fadeIn' : 'fadeOut'](500);
     });
+
+    // The color tray has two modes (setting and bbcode).
     $('.colorCode').click(function() {
       if (ui.colorPicker == 'bbcode') {
         insertBBCode('color', this.title);
@@ -139,27 +180,39 @@ var ui = {
         chat.setSetting('textColor', this.title);
       }
     });
+
+    // Clear the text color setting.
     $('#settings-textColorClear').click(function() {
       chat.setSetting('textColor', null);
       ui.setTextColorPicker(null);
     });
+
+    // Listen for changes in the style menu.
     $('#styleSelection').change(
       function() { ui.setStyle($(this).val()); }
     );
+
+    // Instantly save changed settings in the cookie.
     $('#settingsContainer input.settings').change(function() {
       var value = this.type == 'checkbox' ? this.checked : this.value;
       chat.setSetting(this.id.substring('settings-'.length), value);
     });
+
+    // /quit button.
     $('#logoutButton').click(function() {
       chat.commands.quit();
     });
 
+    // scrolling up the chat list turns off auto-scrolling.
     this.dom.chatList.scroll(function() {
       ui.checkAutoScroll();
       return true;
     });
   },
 
+  /**
+   * Instantly delete the entire message log.
+   */
   clearMessages: function() {
     this.messages = [];
     this.messageId = 0;
@@ -167,6 +220,12 @@ var ui = {
     this.dom.chatList.html('');
   },
 
+  /**
+   * Change the connection status:
+   * - unset the online list when leaving a room.
+   * - change the status icon.
+   * - toggle between login form and room selection menu.
+   */
   setStatus: function(status) {
     // status options are: online, waiting, offline, prejoin.
     if (status == 'prejoin') {
@@ -178,6 +237,9 @@ var ui = {
     this.dom.roomContainer[status == 'online' ? 'fadeIn' : 'fadeOut'](500);
   },
 
+  /**
+   * Change the active stylesheet.
+   */
   setStyle: function(style) {
     config.settings.activeStyle = style;
     this.dom.styleSheets.each(function() {
@@ -186,6 +248,9 @@ var ui = {
     chat.saveSettings();
   },
 
+  /**
+   * This changes the value and appearance of the persistent color button.
+   */
   setTextColorPicker: function(color) {
     $('#settings-textColor')
       .css('color', color || '#FFFFFF')
@@ -195,6 +260,9 @@ var ui = {
     $('#settings-textColorClear').css('display', color ? 'inline-block' : 'none');
   },
 
+  /**
+   * Close the active sidebar and (if needed) open a different one.
+   */
   toggleMenu: function(newMenu, init) {
     var oldMenu = init ? null : config.settings.activeMenu;
     if (oldMenu) this.dom.menu[oldMenu].animate({width: 'hide'}, 'slow');
@@ -219,14 +287,28 @@ var ui = {
     chat.saveSettings();
   },
 
+  /**
+   * Create an informational message.
+   * The first two arguments are passed straight to visual.formatText().
+   *
+   * @param {string} text The message to display (should be in strings.js)
+   * @param {Object} variables (optional) The variables to insert into the text.
+   * @param {string} classes (optional) Any classes (space-separated) to add.
+   *                 The common ones are verbose (message will be suppressed if
+   *                 verbosity is off) or error (message will be colored red).
+   */
   messageAddInfo: function(text, variables, classes) {
+    // If the second argument is a string, we skipped the variables.
     if (!classes && typeof variables == 'string') {
       classes = variables;
       variables = false;
     }
+
+    // Suppress verbose messages.
     if (0 <= (' ' + classes + ' ').indexOf(' verbose ')) {
       if (!config.settings.verbose) return;
     }
+
     text = visual.formatText(text, variables);
     var message = visual.formatMessage({
       body: text,
@@ -237,6 +319,13 @@ var ui = {
     return message;
   },
 
+  /**
+   * Append a delayed (room history) message.
+   * If the message hash already exists, ignore it. This is to prevent rooms
+   * from readding their history when rejoining without clearing the screen.
+   *
+   * @param {Object} message. Must have user, time, room, body and hash keys.
+   */
   messageDelayed: function(message) {
     var entry = visual.formatMessage(message);
     if (!this.messageHash[entry.hash]) {
@@ -251,8 +340,13 @@ var ui = {
     }
   },
 
+  /**
+   * Insert a (rendered) message at an arbitrary point (using the timestamp).
+   *
+   * This is only used for delayed messages.
+   */
   messageInsert: function(message) {
-        var c = this.messages.length;
+    var c = this.messages.length;
     if (message.timestamp < this.messages[0].timestamp) {
       this.messages[0].html.before(message.html);
       this.messages = [message].concat(this.messages);
@@ -269,6 +363,9 @@ var ui = {
     this.scrollDown();
   },
 
+  /**
+   * Append a rendered message to the end of the chat list.
+   */
   messageAppend: function(message) {
     this.messageHash[message.hash] = true;
     this.messages[this.messages.length] = message;
@@ -279,6 +376,9 @@ var ui = {
     this.scrollDown();
   },
 
+  /**
+   * Refresh the room selection menu.
+   */
   refreshRooms: function(rooms) {
     var room = this.dom.roomSelection.val();
     $('option', this.dom.roomSelection).remove();
@@ -289,6 +389,9 @@ var ui = {
     this.dom.roomSelection.html(options).val(room);
   },
 
+  /**
+   * Add a user to the online list.
+   */
   userAdd: function(user, animate) {
     var userLink = $('<div class="row">' + visual.formatUser(user) + '</div>');
 
@@ -301,6 +404,9 @@ var ui = {
     this.userLinks[user.nick] = userLink;
   },
 
+  /**
+   * Remove a user from the online list.
+   */
   userRemove: function(user) {
     if (this.userLinks[user.nick]) {
       this.userLinks[user.nick].slideUp(1000).remove();
@@ -308,6 +414,9 @@ var ui = {
     }
   },
 
+  /**
+   * Remove the online list with a new roster, and set the room selection menu.
+   */
   updateRoom: function(room, roster) {
     var self = this;
     this.dom.roomSelection.val(room);
@@ -322,6 +431,13 @@ var ui = {
     });
   },
 
+  /**
+   * Helper function: Route a keystroke to a callback function.
+   *
+   * @param {Object} callbacks. Functions for each keycode to listen for.
+   *                 Should return true if the event should be terminated.
+   * @return true if the event should be terminated.
+   */
   onKeyMap: function(callbacks) {
     return function(e) {
       if (callbacks[e.keyCode] && callbacks[e.keyCode](e, this)) {
@@ -336,6 +452,10 @@ var ui = {
     }
   },
 
+  /**
+   * Recalculate the input field length, and update the counter.
+   * When a maximum length is set, count down to it.
+   */
   updateMessageLengthCounter: function() {
     var length = this.dom.inputField.val().length;
     if (config.ui.maxMessageLength) {
@@ -346,6 +466,9 @@ var ui = {
     else this.dom.messageLengthCounter.text(this.dom.inputField.val().length);
   },
 
+  /**
+   * Scroll to the bottom of the chat list if autoscrolling is enabled.
+   */
   scrollDown: function() {
     // Only autoscroll if we are at the bottom.
     if(this.autoScroll) {
@@ -355,6 +478,12 @@ var ui = {
     }
   },
 
+  /**
+   * Recalculate auto-scrolling mode: If the user initiated the scroll event
+   * (determined by this.autoScrolled being false), and the view is >=1/3 of
+   * a screen from the bottom (magic number), then auto-scrolling should
+   * be disabled.
+   */
   checkAutoScroll: function() {
     if (this.autoScrolled) return;
     var chatListHeight = parseInt($(this.dom.chatList).css('height'));
