21495bcd2f5d92ec7b59c494971b252aa10ece3b
Christoph Burschka <christoph@burschka.de>
2014-02-09 18:26:26 +0100

    Install in debug mode, preserving JS and CSS.
    The extra bandwidth is minimal, but it allows much easier diagnosis
    for Javascript problems that may occur.

--- calref-install.sh
+++ calref-install.sh
@@ -8,6 +8,7 @@ echo "Configuring cadence"
   --session-auth="https://calref.net/xmpp-auth.php" \
   --chatbot="Ligrev" \
   --title="Ligrev's Lounge" \
+  --mode=debug \
   $*
 
 echo "Building cadence"
