cburschka/cadence

commit af283f74cf7367a8d7380b06eff8ca742e8ec4c7

Cleanup: use jidFromRoomNick, remove old comment.

js/core/xmpp.js
@@ -75,7 +75,7 @@ var xmpp = {
   msg: function(nick) {
     return $msg({
       from: this.connection.jid,
-      to:   Strophe.escapeNode(this.room.current) + '@' + config.xmpp.mucService + (nick ? '/' + nick : ''),
+      to:   jidFromRoomNick(this.room.current, nick),
       type: (nick ? 'chat' : 'groupchat')
     });
   },
@@ -300,7 +300,7 @@ var xmpp = {
   presence: function(room, nick, attrs) {
     return this.pres()
       .attrs({
-        to:Strophe.escapeNode(room) + '@' + config.xmpp.mucService + (nick ? '/' + nick : '')
+        to:this.jidFromRoomNick(room, nick)
       })
       .attrs(attrs);
   },
@@ -429,9 +429,6 @@ var xmpp = {
 
   /**
    * Query the server for rooms and execute a callback.
-   *
-   * This function is actually a callback-wrapper (see xmpp.initialize() ),
-   * which is replaced with its own return value on initialization.
    *
    * @param {function} callback The function to execute after the server responds.
    */

commit a97a9233acd611bb161b3f9562b4f7324b583333

Regression in af283f74: missing a "this."

js/core/xmpp.js
@@ -76,7 +76,7 @@ var xmpp = {
   msg: function(nick) {
     return $msg({
       from: this.connection.jid,
-      to:   jidFromRoomNick(this.room.current, nick),
+      to:   this.jidFromRoomNick(this.room.current, nick),
       type: (nick ? 'chat' : 'groupchat')
     });
   },