a5d4bc4fa4902c8100fc6e5979a40bf5b4d41acf
Christoph Burschka <christoph@burschka.de>
2015-04-29 18:54:14 +0200

    Fix #189: Block emoticons inside "<a>" elements.
    URLs, like code, are particularly prone to containing character sequences
    that might be misidentified as emoticons. To rectify this, "<a>" elements
    now block emoticons in their anchor text.

--- js/core/visual.js
+++ js/core/visual.js
@@ -261,7 +261,7 @@ visual = {
             + config.markup.emoticons[set].codes[code]
             + '" title="' + code + '" alt="' + code + '" /><span class="emote-alt">' + code + '</span>';
     }
-    jq.add('*', jq).not('code, code *').replaceText(this.emoticonRegex, function() {
+    jq.add('*', jq).not('code, code *, a, a *').replaceText(this.emoticonRegex, function() {
       for (var i = 1; i < Math.min(arguments.length-2, emoticonSets.length+1); i++) {
         if (arguments[i]) {
           return emoticonImg(emoticonSets[i-1], arguments[i]);
