3a1f0b82fff5708afb83e4d99e1b50f086cb8b00
Sylae Corell <sylae@calref.net>
2015-06-07 10:55:53 -0600

    Fix duplicate sound entries

--- js/core/config.tpl.js
+++ js/core/config.tpl.js
@@ -134,14 +134,12 @@ var config = {
 
   // Defines the available sounds loaded on chat start:
   sounds: [
-    'droneriots',
     'sound_1',
     'sound_2',
     'sound_3',
     'sound_4',
     'sound_5',
     'sound_6',
-    'yay',
     'pidgin_alert',
     'pidgin_login',
     'pidgin_logout',
